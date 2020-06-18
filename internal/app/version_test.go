c47b1d259c1392f6b9ba156e2a22dcb92f7fe733
murakamiii <9khc727ru7@gmail.com>
2020-06-19 02:51:02 +0900

    add test

--- internal/app/version_test.go
+++ internal/app/version_test.go
@@ -62,3 +62,56 @@ func TestGetiOSVersion(t *testing.T) {
 		}
 	}
 }
+
+func TestApp_GetAndroidVersion(t *testing.T) {
+	tests := []struct {
+		client    *http.Client
+		want    string
+		wantErr bool
+	}{
+		{
+			NewTestClient(func(req *http.Request) *http.Response {
+				return &http.Response{
+					StatusCode: 200,
+					Body:       ioutil.NopCloser(bytes.NewBufferString(htmlMock)),
+				}
+			}),
+			"4.14.2",
+			false,
+		},
+		{
+			NewTestClient(func(req *http.Request) *http.Response {
+				return &http.Response{
+					StatusCode: 500,
+					Body:       ioutil.NopCloser(bytes.NewBufferString("")),
+				}
+			}),
+			"",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		app := App{ Client: tt.client }
+		got, err := app.GetAndroidVersion("com.test")
+		if (err != nil) != tt.wantErr {
+			t.Errorf("App.GetAndroidVersion() error = %v, wantErr %v", err, tt.wantErr)
+			return
+		}
+		if got != tt.want {
+			t.Errorf("App.GetAndroidVersion() = %v, want %v", got, tt.want)
+		}
+	}
+}
+
+const htmlMock = `<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title>title</title>
+      <link rel="stylesheet" href="style.css">
+      <script src="script.js"></script>
+   </head>
+   <body>
+      <div class="hAyfc"><div class="BgcNfc">現在のバージョン</div><span class="htlgb"><div class="IQ1z0d"><span class="htlgb">4.14.2</span></div></span></div>
+   </body>
+</html>`
